110f98fb215cb8722fe41a650f669596573da919
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-01-24 22:43:26 +0400

    More sidebar menu options added

--- src/app/layout/Sidebar.tsx
+++ src/app/layout/Sidebar.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
-import { Box, Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
+import { Drawer, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Toolbar, Typography } from "@mui/material";
 import { NavLink } from "react-router-dom";
 import AppName from "./AppName";
-import { Person, Favorite, ShoppingBasket, Loop } from '@mui/icons-material'; // Import the icons you want to use
+import { Person, Favorite, ShoppingBasket, Loop, Collections, ThumbUp, SettingsApplications, Receipt, Reviews, CommentBank, AddAlarm, Assistant, SupportAgent, ManageHistory, SavedSearch, Chat, Visibility, Summarize } from '@mui/icons-material'; // Import the icons you want to use
 
 const drawerWidth = '240px';
 
@@ -30,6 +29,14 @@ export default function Sidebar({ handleThemeChange, darkMode }: Props) {
       </Toolbar>
       {/* This creates space for the Header */}
       <List>
+        <ListItem key="Summary" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Summarize />
+            </ListItemIcon>
+            <ListItemText primary="Summary" />
+          </ListItemButton>
+        </ListItem>
         <ListItem key="Profile" disablePadding>
           <ListItemButton component={NavLink} to="/customer/create">
             <ListItemIcon>
@@ -61,6 +68,102 @@ export default function Sidebar({ handleThemeChange, darkMode }: Props) {
               <Loop />
             </ListItemIcon>
             <ListItemText primary="Returns" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Collections" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Collections />
+            </ListItemIcon>
+            <ListItemText primary="Collections" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Likes" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ThumbUp />
+            </ListItemIcon>
+            <ListItemText primary="Likes" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Reviews" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Reviews />
+            </ListItemIcon>
+            <ListItemText primary="Reviews" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Invoices" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Receipt />
+            </ListItemIcon>
+            <ListItemText primary="Invoices" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Comments" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <CommentBank />
+            </ListItemIcon>
+            <ListItemText primary="Comments" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Settings" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SettingsApplications />
+            </ListItemIcon>
+            <ListItemText primary="Settings" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Support" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SupportAgent />
+            </ListItemIcon>
+            <ListItemText primary="Support" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="History" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <ManageHistory />
+            </ListItemIcon>
+            <ListItemText primary="History" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Saved Searches" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <SavedSearch />
+            </ListItemIcon>
+            <ListItemText primary="Saved Searches" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Messages" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Chat />
+            </ListItemIcon>
+            <ListItemText primary="Messages" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Watch items" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Visibility />
+            </ListItemIcon>
+            <ListItemText primary="Watch items" />
+          </ListItemButton>
+        </ListItem>
+        <ListItem key="Recommendations" disablePadding>
+          <ListItemButton>
+            <ListItemIcon>
+              <Assistant />
+            </ListItemIcon>
+            <ListItemText primary="Recommendations" />
           </ListItemButton>
         </ListItem>
       </List>
