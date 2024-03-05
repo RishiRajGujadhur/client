c6dc0f42f1a4b2a2c804080d3b0d88b8c7cd1a2a
Rishi Raj Gujadhur <r.r.gujadhur@gmail.com>
2024-03-05 18:52:15 +0400

    SignalR integration draft

--- src/features/home/Homepage.tsx
+++ src/features/home/Homepage.tsx
@@ -1,8 +1,28 @@
+import { HubConnectionBuilder } from '@microsoft/signalr'
 import { Box, Typography } from '@mui/material';
- 
+import { toast } from 'react-toastify';
 
 export default function HomePage() {
- 
+
+    // const connection = new HubConnectionBuilder()
+    // .withUrl("http://localhost:5227/notifications")
+    // .build();
+  
+    // connection.on("ReceiveMessage", (message) => {
+    //   toast.success(`Received message: ${message}`);
+    //   console.log(`Received message: ${message}`);
+    // });
+  
+    // connection.start().then(() => {
+    //   toast.success("SignalR connection established.");
+    //   console.log("SignalR connection established.");
+    // }).catch((error) => {
+    //   toast.error("SignalR connection failed: ", error);
+    //   console.error("SignalR connection failed: ", error);
+    // });
+  
+    // connection.invoke("SendMessage", "Hello from React!");
+
     return (
         <>
             
